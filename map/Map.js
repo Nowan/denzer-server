3cb5df214b8a880d54febfb5b091007a4d7e22e3
Mykyta Dniprovskyi <dnipromv@gmail.com>
2018-11-20 03:48:45 +0100

    update map & send concrete values to client

--- map/Map.js
+++ map/Map.js
@@ -4,16 +4,26 @@ const rawData = require("./data/map.json");
 
 class Map {
     constructor() {
-        this.terrain = _getTerrain();
-        this.spawnPoints = _getSpawnPoints();
+        this.tileSize = rawData.tilewidth;
+        this.width = rawData.width * this.tileSize;
+        this.height = rawData.height * this.tileSize;
+        this.terrain = _parseTerrain();
+        this.spawnPoints = _parseSpawnPoints();
     }
 }
 
-function _getTerrain() {
-    return _findLayer("Terrain").data;
+function _parseTerrain() {
+    const terrainLayer = _findLayer("Terrain");
+    const terrain = {
+        columns: terrainLayer.width,
+        rows: terrainLayer.height,
+        tiles: terrainLayer.data
+    };
+    
+    return terrain;
 }
 
-function _getSpawnPoints() {
+function _parseSpawnPoints() {
     return _findLayer("Spawn").objects.map((spawnPoint) => {
         return { x: spawnPoint.x + spawnPoint.width * 0.5, y: spawnPoint.y + spawnPoint.height * 0.5 };
     });
